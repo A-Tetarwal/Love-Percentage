64c0ca395627bf44e41cf6abaaebaa24a0a9ab26
A-Tetarwal <andashishwill@gmail.com>
2024-08-14 09:39:07 +0530

    Love UI

---
+++ Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className='bg-pink-500 px-5 py-5 w-screen flex justify-between items-center mb-10'>
+        <h1 className='font-extrabold text-3xl'>U&I❤️‍🔥</h1>
+        <Link href='/Letters' className='font-mono'>Letters</Link> {/* routing ke liye app me folder banana hai */}
+    </div>
+  )
+}
+
+export default Header
